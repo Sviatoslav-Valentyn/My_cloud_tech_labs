a46467191432ea204cfb75e4e7007b49638d7c87
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2023-04-12 23:59:06 +0300

    fff

--- lab1/main.tf
+++ lab1/main.tf
@@ -14,16 +14,16 @@ module "instruction_table" {
 }
 
 //lab2
-module "lambda_api" {
-  source = "../modules/lambda-api"
-
-  function_name = module.naming.id
-  handler       = "index.handler"
-  lambda_zip    = "${path.module}../lambda-api.zip"
-  context       = module.naming.context
-}
-
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
-}
+//  module "lambda_api" {
+//  source = "../modules/lambda-functions"
+//
+//  function_name = module.naming.id
+//  handler       = "index.handler"
+//  lambda_zip    = "${path.module}../lambda-api.zip"
+//  context       = module.naming.context
+//}
+//
+//resource "aws_iam_role_policy_attachment" "test-attach" {
+//  role       = aws_iam_role.role.name
+//  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+//}
